61961bfc21fcc02c45c62b526f156150fe9c4f30
Mohammad Ali Shohag <47399558+shohag14748@users.noreply.github.com>
2020-10-22 22:45:45 +0600

    indexfile upload

---
+++ assets/js/main.js
@@ -0,0 +1,68 @@
+$(document).ready(function () {
+    $('body').append("<span class='sidebar-open-overlay'></span>");
+    $('.topbar .submenu-link').click(function (e) {
+        e.preventDefault();
+        if ($(window).width() < 1140) {
+            $(this).siblings().slideToggle().parent().siblings().children('.sub-menu').slideUp();
+        }
+    })
+    $('.menu-toggler-btn').click(function (e) {
+        e.preventDefault();
+        if ($(window).width() < 1140) {
+            $('.main-menu .menu').addClass('active');
+            $('.sidebar-open-overlay').addClass('active');
+        }
+    })
+    $('.navigation .close-btn,.sidebar-open-overlay').click(function (e) {
+        e.preventDefault();
+        $('.main-menu .menu').removeClass('active');
+        $('.sidebar-open-overlay').removeClass('active');
+    })
+    $('.cart-toggler-btn').click(function (e) {
+        e.preventDefault();
+        $('.shopping-bag').addClass('active');
+        $('.sidebar-open-overlay').addClass('active');
+    })
+    $('.shopping-bag .close-btn,.sidebar-open-overlay').click(function (e) {
+        e.preventDefault();
+        $('.shopping-bag').removeClass('active');
+        $('.sidebar-open-overlay').removeClass('active');
+    })
+    $(window).scroll(function(){
+        if($(window).scrollTop() > 100){
+            $('.main-menu').addClass('scroll');
+        }else{
+            $('.main-menu').removeClass('scroll');
+        }
+    })
+
+    $('.banner-carousel').owlCarousel({
+        loop: true,
+        margin: 0,
+        nav: false,
+        dots:false,
+        items: 1,
+        autoplay:true,
+        autoplayTimeout:5000,
+        autoplaySpeed:500,
+        
+    })
+    $('.topCategories-carousel').owlCarousel({
+        loop:true,
+        margin:0,
+        nav:true,
+        dots:false,
+        autoplay:true,
+        autoplayTimeout:5000,
+        autoplaySpeed:300,
+        navText: ["<i class='icon-angle-left'></i>", "<i class='icon-angle-right'></i>"],
+        responsive:{
+            0:{
+                items:2
+            },
+            768:{
+                items:6
+            }
+        }
+    })
+});
